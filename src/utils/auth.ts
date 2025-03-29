3501ed2c3e6c4b5e966ccf145ca8a50e2f45417e
francis1009 <francism56789@gmail.com>
2025-03-29 15:28:25 +0800

    Test auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -12,35 +12,35 @@ export function serverAuth(env: Env) {
 				}),
 				type: "sqlite",
 			},
-			user: {
-				modelName: "user",
-				fields: {
-					id: "user_id",
-					name: "username",
-					image: "profile_image_url",
-					createdAt: "created_at",
-					updatedAt: "last_updated",
-				},
-				additionalFields: {
-					latitude: {
-						type: "number",
-						required: false,
-					},
-					longitude: {
-						type: "number",
-						required: false,
-					},
-					description: {
-						type: "string",
-						required: false,
-					},
-					is_petsitter: {
-						type: "number",
-						required: false,
-						default: 0,
-					},
-				},
-			},
+			// user: {
+			// 	modelName: "user",
+				// fields: {
+				// 	id: "user_id",
+				// 	name: "username",
+				// 	image: "profile_image_url",
+				// 	createdAt: "created_at",
+				// 	updatedAt: "last_updated",
+				// },
+				// additionalFields: {
+				// 	latitude: {
+				// 		type: "number",
+				// 		required: false,
+				// 	},
+				// 	longitude: {
+				// 		type: "number",
+				// 		required: false,
+				// 	},
+				// 	description: {
+				// 		type: "string",
+				// 		required: false,
+				// 	},
+				// 	is_petsitter: {
+				// 		type: "number",
+				// 		required: false,
+				// 		default: 0,
+				// 	},
+				// },
+			// },
 			baseURL: env.BETTER_AUTH_URL,
 			socialProviders: {
 				google: {
