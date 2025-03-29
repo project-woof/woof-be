5fc5ec20fd5ecad1221b69ff7ab91c060727e4ff
francis1009 <francism56789@gmail.com>
2025-03-29 16:59:05 +0800

    Fix databse issue with better-auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { D1Dialect } from "kysely-d1";
-import { jwt, bearer } from "better-auth/plugins";
+// import { jwt, bearer } from "better-auth/plugins";
 
 let auth: ReturnType<typeof betterAuth>;
 export function serverAuth(env: Env) {
@@ -12,35 +12,34 @@ export function serverAuth(env: Env) {
 				}),
 				type: "sqlite",
 			},
-			// user: {
-			// 	modelName: "user",
-				// fields: {
-				// 	id: "user_id",
-				// 	name: "username",
-				// 	image: "profile_image_url",
-				// 	createdAt: "created_at",
-				// 	updatedAt: "last_updated",
-				// },
-				// additionalFields: {
-				// 	latitude: {
-				// 		type: "number",
-				// 		required: false,
-				// 	},
-				// 	longitude: {
-				// 		type: "number",
-				// 		required: false,
-				// 	},
-				// 	description: {
-				// 		type: "string",
-				// 		required: false,
-				// 	},
-				// 	is_petsitter: {
-				// 		type: "number",
-				// 		required: false,
-				// 		default: 0,
-				// 	},
-				// },
-			// },
+			user: {
+				modelName: "user",
+				fields: {
+					name: "username",
+					image: "profile_image_url",
+					createdAt: "created_at",
+					updatedAt: "last_updated",
+				},
+				additionalFields: {
+					latitude: {
+						type: "number",
+						required: false,
+					},
+					longitude: {
+						type: "number",
+						required: false,
+					},
+					description: {
+						type: "string",
+						required: false,
+					},
+					is_petsitter: {
+						type: "number",
+						required: false,
+						default: 0,
+					},
+				},
+			},
 			baseURL: env.BETTER_AUTH_URL,
 			socialProviders: {
 				google: {
@@ -48,7 +47,7 @@ export function serverAuth(env: Env) {
 					clientSecret: env.GOOGLE_CLIENT_SECRET!,
 				},
 			},
-			plugins: [jwt(), bearer()],
+			// plugins: [jwt(), bearer()],
 		});
 	}
 	return auth;
