38da28c36d5680ae9dd8cd06a749a59b8773328c
francis1009 <francism56789@gmail.com>
2025-04-11 17:56:30 +0800

    Fix generateUUID

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -2,5 +2,5 @@ import { v7 as uuidv7 } from "uuid";
 
 export const generateUUID = (parameter: string): string => {
 	const generatedUUID = uuidv7().replace(/-/g, "");
-	return `${parameter}_${generatedUUID};`;
+	return `${parameter}_${generatedUUID}`;
 };
