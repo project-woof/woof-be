68192b8363064f3a56800d82b62fccecd2deccef
francis1009 <francism56789@gmail.com>
2025-04-17 12:07:57 +0800

    Replace @cloudflare/workers-types with cloudflare:workers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-/// <reference types="@cloudflare/workers-types" />
-
+import { env } from "cloudflare:workers";
 import { handleRequest } from "@/routes/routes";
 import { serverAuth } from "@/utils/auth";
 export { PetsitterDO } from "@/services/doService";
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
+	async fetch(request: Request, ctx: ExecutionContext): Promise<Response> {
 		const auth = serverAuth(env);
 		return handleRequest(request, env, auth);
 	},
